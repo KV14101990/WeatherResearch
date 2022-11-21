d815385058a18f94f0a924bf7dea24cb27237a85
Khushboo Vimal <khushboo.vimal@gds.ey.com>
2022-11-21 13:47:21 +0000

    natural key in snapshot

--- snapshots/scd_raw_listings.sql
+++ snapshots/scd_raw_listings.sql
@@ -2,7 +2,7 @@
     {{
         config(
             target_schema='raw',
-            unique_key='id',
+            unique_key="id||'-'||name",
             strategy='timestamp',
             updated_at='updated_at',
             invalidate_hard_deletes=True
@@ -10,4 +10,7 @@
     }}
 
     select * from {{ source('airbnb','listings') }}
+
+ /*   select transaction_id || '-' || line_item_id as key ,* from {{ source('airbnb', 'listings') }} */
+
  {% endsnapshot %}
